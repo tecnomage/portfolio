24a9c385f593ac7995e5f515b397f4a9b38c2e03
tecnomage <tecnocratay@gmail.com>
2023-12-05 14:45:18 -0300

    title change

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -37,7 +37,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
           />
           <link rel="icon" type="image/png" href={"/favicon.png"} />
-          <title>Portfolio sample</title>
+          <title>Vinicius Braga</title>
         </Head>
 
         <body>
